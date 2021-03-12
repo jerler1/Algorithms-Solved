b6e181ca65df1f551bb116b5eadfe07022fb3fe7
Bunks <busk_1@yahoo.com>
2021-03-11 22:10:55 -0500

    converted bubble sort to javascript and it's working

--- Bubble Sort/BubbleSort.js
+++ Bubble Sort/BubbleSort.js
@@ -1,8 +1,21 @@
 // Complete the countSwaps function below.
 function countSwaps(a) {
-console.log(a);
 
+    let swaps = 0;
+    for (let i = 0; i < a.length; i++) {
+    
+        for (let j = 0; j < a.length - 1; j++) {
+            // Swap adjacent elements if they are in decreasing order
+            if (a[j] > a[j + 1]) {
+                let temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+                swaps++;
+            }
+        }
+        
+    }
 }
 
-let a = [1,2,3];
+let a = [18, 80, 9, 31, 99, 1];
 countSwaps(a);
